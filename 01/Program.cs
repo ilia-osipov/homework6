1f3a65ec90b9638c60f80d093b7c88d69bd3ed0d
Marina <marinosharipo@gmail.com>
2022-09-12 13:27:05 +0700

    added

---
+++ 01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите любое количество чисел через пробел: ");
+string numbers = Console.ReadLine();
+
+int[] array = numbers.Split(' ').Select(int.Parse).ToArray();
+Console.WriteLine($"Чисел больше 0: {Positive(array)}");
+
+int Positive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) count += 1;
+    }
+    return count;
+}
---
+++ 02/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Clear();
+Console.WriteLine("Введите B1: ");
+double b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите K1: ");
+double k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите B2: ");
+double b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите K2: ");
+double k2 = int.Parse(Console.ReadLine());
+
+double x = GetPointX(b1, k1, b2, k2);
+if (k1 != k2)
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({GetPointX(b1, k1, b2, k2)}, {GetPointY(b1, k1, b2, k2)})");
+else Console.WriteLine("Линии параллельны");
+
+double GetPointX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+double GetPointY(double b1, double k1, double b2, double k2)
+{
+    double y = k1 * x + b1;
+    return y;
+}
